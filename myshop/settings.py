963fbdd30195245fc50cf0354249a6b03908e280
barani-2502 <barani@testpress.in>
2025-09-18 12:45:30 +0530

    Create an app cart, data structure, views, urls and forms for it

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 #India number formatting
 USE_THOUSAND_SEPARATOR = True
 NUMBER_GROUPING = (3,2,0)
+
+CART_SESSION_ID = 'cart'
